dbb173906eda1a68d62cd7538f0472a6cb76d111
acty123 <johan.andres.garzon@correounivalle.edu.co>
2019-08-15 17:05:44 -0500

    Updates

---
+++ precache-manifest.d38e800c56f31545e978e80b50baed46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf1ef9812ac64831a678bb4cf658616",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "34f920518eb6ca8f78ba",
+    "url": "/monsters-rolodex/static/css/main.8eb78daa.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "34f920518eb6ca8f78ba",
+    "url": "/monsters-rolodex/static/js/main.62d20e59.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
